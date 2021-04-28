514cb59a5c34e03c34c6f158728bad4664ec559c
Vivien Bojtos <Vivien.Bojtos@ibm.com>
2021-04-28 16:01:56 +0200

    chore: remove common tekton tasks from .ci

--- scripts/check.sh
+++ scripts/check.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 defs=$(mktemp)
 
 echo "Linting CD Pipeline"
-yq r -j .bluemix/tekton.yaml | jq -r '.inputs[] | select(.service=="${TEKTON_CATALOG_REPO}") | "'$COMMON_TEKTON_TASKS_DIR'/" + .path + "/*.yaml"' | tee "$defs"
 yq r -j .bluemix/tekton.yaml | jq -r '.inputs[] | select(.service=="${TEKTON_DEFINITION_REPO}") | .path + "/*.yaml"' | tee -a "$defs"
 
 tekton-lint $(cat "$defs")
